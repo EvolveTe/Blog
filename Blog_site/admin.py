aa99facd46e5fda1c783ed7d0b965a5527f7db6b
Mahan mahmodi <87831227+mahancrx@users.noreply.github.com>
2023-10-13 15:19:00 +0330

    DATE BASE EDITE 1

--- Blog_site/admin.py
+++ Blog_site/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from Blog_site.models import *
+
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('title', 'author', 'views', 'status', 'created_date', 'updated_date', 'published_date')
+    list_filter = ('status', 'author',)
+    # ordering = ('-created_date',)
+    search_fields = ('title', 'author', 'content')
+
+
+admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
